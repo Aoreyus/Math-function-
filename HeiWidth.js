4dda3c30a02ff0e30840378fb29a8e42dc56cc38
Aoreyus <30877980+Aoreyus@users.noreply.github.com>
2017-08-23 10:20:41 -1000

    Add files via upload

---
+++ HeiWidth.js
@@ -0,0 +1,21 @@
+function calArea(width,height){
+
+var area = width*height;
+
+return area;
+
+// return shows the product(answer)
+
+}
+
+var x = prompt("Enter height");
+
+var z = prompt("Enter width");
+
+// subsituted x and y for height and width
+
+var wallOne= calArea(x,z);
+
+// call the wallOne function
+
+window.alert(wallOne);

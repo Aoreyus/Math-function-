78cb4f3273d9a4f9ffa1672593cc4eb1798ea08b
Aoreyus <30877980+Aoreyus@users.noreply.github.com>
2017-09-01 10:17:49 -1000

    Add files via upload

---
+++ guessinggame.js
@@ -0,0 +1,14 @@
+var user1=prompt("Player 1, chose a number between 1-6");
+if((user1 <1) || (user1>6) || isNaN(user1)){
+window.alert("You cannot follow direction");
+}
+
+else{
+var user2=prompt("Guess a number between 1-6");
+if(user1==user2){
+window.alert("You win!");
+}
+else{
+window.alert("You lost, Player 1 choosed" + " " + user1);
+}
+}

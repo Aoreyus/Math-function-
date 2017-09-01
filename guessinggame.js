c6f89882aecb0db9bee1c4c874a4fbd40df1265d
Aoreyus <30877980+Aoreyus@users.noreply.github.com>
2017-09-01 10:24:01 -1000

    Update guessinggame.js

--- guessinggame.js
+++ guessinggame.js
@@ -1,4 +1,5 @@
-var user1=prompt("Player 1, chose a number between 1-6");
+var user1=prompt("Player 1, chose a number between 1-6"); 
+var user1= Math.floor(parseInt(user1));
 if((user1 <1) || (user1>6) || isNaN(user1)){
 window.alert("You cannot follow direction");
 }
